a779ce49c0fa0e8bd21243c0a8b9927f4439ccee
Maciej Grzybek <maciekgrzybek1@gmail.com>
2024-10-14 17:38:48 +0200

    chore: install playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults, defaultExclude } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, './__e2e__'],
   },
 });
